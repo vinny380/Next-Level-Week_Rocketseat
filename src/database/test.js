6eef2861e4503b27ea0f4866967704d99c7976e6
Vinícius Purgato <vinny.purgato@gmail.com>
2020-08-08 15:49:50 -0300

    sqlite with the 3 tables defined

---
+++ src/database/test.js
@@ -0,0 +1,49 @@
+const Database = require('./db')
+const createProffy = require('./createProffy')
+
+
+Database.then(async (db) => {
+    //insert data
+
+
+    proffyValue = { //this one works for the sqlite table "proffys"
+        name: "Diego Fernandes",
+        avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "9348437173",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+    }
+
+    classValue = { //this one works for the sqlite table class
+        subject: "Química",
+        cost: "20",
+    }
+
+    classScheduleValues = [ //this one works for the sqlite table class_schedule
+        {
+            weekday: 1,
+            time_from: 720,
+            time_to: 1220
+        },
+        {
+            weekday: 0,
+            time_from: 520,
+            time_to: 1220
+        }
+    ]
+
+   //await createProffy(db, {proffyValue, classValue, classScheduleValues})
+   //check inserted datas
+
+   //all of the proffys
+   const selectedProffys = await db.all("SELECT * FROM proffys")
+   //console.log(selectedProffys)
+
+   //check the classes of a selected proffy, and bring the teacher's data with it
+   const selectClassesAndProffys = await db.all(`
+        SELECT classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE classes.proffy_id = 1;
+   `)
+    console.log(selectClassesAndProffys)
+})
